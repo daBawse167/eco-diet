fd10fc227564339417b912b4ae5caecdf4cd7085
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2024-01-13 11:40:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -67,12 +67,12 @@ def create_recommendations():
     #gets the animals with GHG lower than the target
     for option in options:
         if option <= target:
-            low_carbon.append([animals_eaten["Item"][i], option])
+            low_carbon.append([animals_eaten["Item"].iloc[i], option])
         i += 1
         
     #in case no choice is made, get the min red and white meat animals
-    white_meat = ['Chickens', 'Ducks', 'Turkeys']
-    red_meat = ['Cattle', 'Goats', 'Sheep', 'Swine']
+    white_meat = ['Chicken']
+    red_meat = ['Cow', 'Goat', 'Sheep', 'Pig', 'Buffalo']
     
     white_meat_options = [i for i in low_carbon if i[0] in white_meat]
     red_meat_options = [i for i in low_carbon if i[0] in red_meat]
@@ -82,17 +82,16 @@ def create_recommendations():
     
     red_meat_options = pd.DataFrame(red_meat_options).sort_values(ascending=True, by="emissions")
     white_meat_options = pd.DataFrame(white_meat_options).sort_values(ascending=True, by="emissions")
-        
+    
     idx = 0
     recommend_list = {"emitted (kg)":round(total_emitted, 3),
                       "target (kg)":0,
-                     'Chickens': 0,
-                     'Ducks': 0,
-                     'Turkeys': 0,
-                     'Cattle': 0,
-                     'Goats': 0,
+                     'Chicken': 0,
+                     'Buffalo': 0,
+                     'Cow': 0,
+                     'Goat': 0,
                      'Sheep': 0,
-                     'Swine': 0}
+                     'Pig': 0}
     emissions_counter = 0
     white_meat_counter = 0
     red_meat_counter = 0
@@ -100,10 +99,14 @@ def create_recommendations():
     white_meat_max = 340
     red_meat_max = 500
     portion = 85
-
     
     red_idx = 0
     white_idx = 0
+    
+    #make sure we don't break white & red meat's limits
+    white_limit = False
+    red_limit = False
+    
     while emissions_counter < target:
 
         #make sure white and red idx don't get out of range
@@ -112,39 +115,52 @@ def create_recommendations():
         if white_idx>=len(white_meat_options):
             white_idx = 0
         
+        #break if no. grams is exceeded
+        if white_limit:
+            if red_limit:
+                break
         
-        #add 1 portion of white meat & its emissions
-        meat_emission = float(white_meat_options.iloc[white_idx]["emissions"])
-        animal = white_meat_options.iloc[white_idx]["animal"]
-        
-        if emissions_counter+(meat_emission*portion) < target:
-            emissions_counter += meat_emission*portion
-            recommend_list[animal] += portion
+        if white_meat_counter < white_meat_max:
+            #add 1 portion of white meat & its emissions
+            meat_emission = float(white_meat_options.iloc[white_idx]["emissions"])
+            animal = white_meat_options.iloc[white_idx]["animal"]
+
+            if emissions_counter+(meat_emission*portion) < target:
+                emissions_counter += meat_emission*portion
+                white_meat_counter += portion
+                recommend_list[animal] += portion
+            else:
+                #round up to 50% less emissions
+                portion = np.floor((target-emissions_counter)/meat_emission)
+                emissions_counter += meat_emission*portion
+                white_meat_counter += portion
+                recommend_list[animal] += portion
+                break
         else:
-            #round up to 50% less emissions
-            portion = np.floor((target-emissions_counter)/meat_emission)
-            emissions_counter += meat_emission*portion
-            recommend_list[animal] += portion
-            break
+            white_limit = True
         
-        #add 1 portion of red meat & its emissions
-        meat_emission = float(red_meat_options.iloc[red_idx]["emissions"])
-        animal = red_meat_options.iloc[red_idx]["animal"]
-        
-        if emissions_counter+(meat_emission*portion) < target:
-            emissions_counter += meat_emission*portion
-            recommend_list[animal] += portion
+        if red_meat_counter < red_meat_max:
+            #add 1 portion of red meat & its emissions
+            meat_emission = float(red_meat_options.iloc[red_idx]["emissions"])
+            animal = red_meat_options.iloc[red_idx]["animal"]
+
+            if emissions_counter+(meat_emission*portion) < target:
+                emissions_counter += meat_emission*portion
+                red_meat_counter += portion
+                recommend_list[animal] += portion
+            else:
+                #round up to 50% less emissions
+                portion = np.floor((target-emissions_counter)/meat_emission)
+                emissions_counter += meat_emission*portion
+                red_meat_counter += portion
+                recommend_list[animal] += portion
+                break
         else:
-            #round up to 50% less emissions
-            portion = np.floor((target-emissions_counter)/meat_emission)
-            emissions_counter += meat_emission*portion
-            recommend_list[animal] += portion
-            break
+            red_limit = True
         
         
         red_idx += 1
         white_idx += 1
-      
     
     recommend_list["target (kg)"] = round(emissions_counter, 3)
     
@@ -177,82 +193,39 @@ def create_recommendations():
 
 
 def find_stock(country_name="", eaten={}):
-    #finds stocks for cattle and swine
-    cleaned_data = clean_data()
-    final_df, stock = cleaned_data[0], cleaned_data[1]
-
-    country_df = final_df[final_df["Area"]==country_name]
-    country_stocks = stock[stock["Area"]==country_name]
-    cattle_stocks = sum(country_stocks[stock["Item"]=="Cattle"]["Value"])
-    swine_stocks = sum(country_stocks[stock["Item"]=="Swine"]["Value"])
-
-    #database storing impact of eaten meat
-    no_eaten = []
-    value_list = []
-    stock_list = []
-    per_capita = []
+    #get FAOSTAT data
+    faostat = pd.read_csv("FAOSTAT.csv")
+    faostat = faostat[['Area', 'Item', 'Value']]
+    faostat["emissions (per kg)"] = faostat["Value"]
+    faostat = faostat.drop("Value", axis=1)
     
-    for i in country_df["Item"]:
-        idx = list(country_df["Item"]).index(i)
-
-        #finds stocks for the animals
-        if i != "Cattle":
-            if i != "Swine":
-                stock_list.append(list(country_stocks[country_stocks["Item"]==i]["Value"])[0])
-            else:
-                stock_list.append(swine_stocks)
-        else:
-            stock_list.append(cattle_stocks)
-
-        no_eaten.append(eaten[i])
-        value_list.append(country_df.iloc[idx]["Value (kt)"]*1000000)
-
-        #calculates the emissions of animal per person and weekly
-        per_capita.append(value_list[idx]/stock_list[idx])
-        #total_value.append(per_capita[idx]*eaten[i])
-
-    country_avg_mass = data[data["country"]==country_name]
-    animals_avg_mass = [list(country_avg_mass[i])[0] for i in list(country_df["Item"])]
+    #change animals names
+    faostat["Item"] = faostat["Item"].replace("Meat of cattle with the bone, fresh or chilled", "Cow")
+    faostat["Item"] = faostat["Item"].replace("Meat of goat, fresh or chilled", "Goat")
+    faostat["Item"] = faostat["Item"].replace("Meat of buffalo, fresh or chilled", "Buffalo")
+    faostat["Item"] = faostat["Item"].replace("Meat of sheep, fresh or chilled", "Sheep")
+    faostat["Item"] = faostat["Item"].replace("Meat of chickens, fresh or chilled", "Chicken")
+    faostat["Item"] = faostat["Item"].replace("Meat of pig with the bone, fresh or chilled", "Pig")
     
-    #calculate how much mass in an animal is usable
-    usable_meats = []
-    emissions_per_kg_list = []
+    #loop over country
+    country_df = faostat[faostat["Area"]==country_name]
+    
     emissions_per_gram_list = []
     weekly_emitted = []
     
     for i in list(country_df["Item"]):
+        #get the emissions per gram and weekly emitted
         idx = list(country_df["Item"]).index(i)
+        emissions_per_gram = country_df["emissions (per kg)"].iloc[idx] / 1000
         
-        #get the percentage of usable meat
-        usable = list(usable_meat[i])[0]
-        #get the average mass
-        avg_mass = list(country_avg_mass[i])[0]
-        #calculate out how much mass is used
-        usable_mass = usable*avg_mass
-        usable_meats.append(usable_mass)
-        
-        #find the emissions of one of the animal
-        one_animal_emissions = per_capita[idx]
-        #calculate emissions per kg of the animal
-        emissions_per_kg = one_animal_emissions/usable_mass
-        emissions_per_gram = emissions_per_kg/1000
-        
-        emissions_per_kg_list.append(emissions_per_kg)
         emissions_per_gram_list.append(emissions_per_gram)
-        
-        #find the weekly emissions per animal, given the user's input in grams
-        weekly_emitted.append(emissions_per_gram*eaten[i])
+        weekly_emitted.append(round(emissions_per_gram*eaten[i], 2))
     
-    #puts it all together in one databse
-    animals_eaten = pd.DataFrame({"Item":list(country_df["Item"]), "no. eaten":no_eaten, "Value (kg)":value_list,
-                                  "stock":stock_list, "per capita (GHG kg/animal)":per_capita,
-                                  "average_mass (kg)":animals_avg_mass, "usable_meat (animal kg)":usable_meats,
-                                  "emissions (per kg)":emissions_per_kg_list, "emissions (per gram)":emissions_per_gram_list,
-                                  "weekly_emitted (kg/animal)":weekly_emitted})
-    print(animals_eaten)
-  
-    options = list(animals_eaten["emissions (per gram)"])
-    return [animals_eaten, options]
+    #add the columns to the dataset
+    country_df["emissions (per gram)"] = emissions_per_gram_list
+    country_df["weekly_emitted (kg/animal)"] = weekly_emitted
+    
+    return [country_df, list(country_df["emissions (per gram)"])]
 
 
 
