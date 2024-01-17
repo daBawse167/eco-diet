b793d040bb4d5ef9098db51b0b8add3e919b7992
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2024-01-17 14:52:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -228,5 +228,24 @@ def find_stock(country_name="", eaten={}):
     
     return [country_df, list(country_df["emissions (per gram)"])]
 
+@app.route('/present', methods=["GET"])
+def present(country=""):
+    total_df = pd.read_csv("FAOSTAT.csv")
+    total_df["Item"] = total_df["Item"].replace("Meat of cattle with the bone, fresh or chilled", "Cow")
+    total_df["Item"] = total_df["Item"].replace("Meat of goat, fresh or chilled", "Goat")
+    total_df["Item"] = total_df["Item"].replace("Meat of buffalo, fresh or chilled", "Buffalo")
+    total_df["Item"] = total_df["Item"].replace("Meat of sheep, fresh or chilled", "Sheep")
+    total_df["Item"] = total_df["Item"].replace("Meat of chickens, fresh or chilled", "Chicken")
+    total_df["Item"] = total_df["Item"].replace("Meat of pig with the bone, fresh or chilled", "Pig")
+
+    countries = list(np.unique(total_df["Area"]))
+    animals = list(np.unique(total_df[total_df["Area"]==country]["Item"]))
+    present = []
+
+    for animal in ['Buffalo', 'Chicken', 'Cow', 'Goat', 'Pig', 'Sheep']:
+        present.append(animal in animals)
+
+    return {'Buffalo':present[0], 'Chicken':present[1], 'Cow':present[2], 'Goat':present[3], 'Pig':present[4], 'Sheep':present[5]}
+
 if __name__=="__main__":
     app.run(debug=True)
