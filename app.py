907e02de40713838bb285bf018be4ca536b62bb1
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2024-01-24 17:24:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -230,8 +230,8 @@ def create_recommendations():
                                 "https://i.postimg.cc/QCc94kf0/buffalo.png", "https://i.postimg.cc/50RT78XY/goat.png",
                                 "https://i.postimg.cc/NjGh0Gv0/sheep.png", "https://i.postimg.cc/3xqLH3KV/pig.png"]
     
-    return_df = {"recommend_grams":return_df["recommend_grams"], "recommend_e":return_df["recommend_e"], "eaten":return_df["eaten"],
-                 "emitted":return_df["emitted"], "images":return_df["images"]}
+    return_df = {"recommend_grams":list(return_df["recommend_grams"]), "recommend_e":list(return_df["recommend_e"]), "eaten":list(return_df["eaten"]),
+                 "emitted":list(return_df["emitted"]), "images":list(return_df["images"])}
     
     return return_df
 
