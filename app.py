73ecccc9f4b42cf9310fbd824f2fecd90bfe5d09
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2024-04-14 17:52:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ def create_recommendations(eaten, country_name, favourites, percent_reduction,
             else:
                 red_meat_counter += meat_emission
         else:
-            idx = list(user_chosen_dishes).index(i)
+            idx = (user_chosen_dishes.T)[0].index(meat)
             del user_chosen_dishes[idx]
             break
 
