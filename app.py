94bae0bd5b9537b77b6d71dbd9327d8a9b06bde5
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2024-06-20 16:02:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -307,9 +307,6 @@ def create_recommendations(eaten, country_name, favourites, percent_reduction,
     no_dishes_chosen=0
     
     return_animals = find_stock(country_name=country_name, eaten=eaten)
-
-    for i in return_animals.columns:
-        print(i, return_animals[i])
                                
     animals_eaten, options = return_animals[0], return_animals[1]
     user_chosen_dishes = []
