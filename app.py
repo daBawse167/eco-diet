c584044dc431364b1d64d9f321ea5969ffc5fdfc
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2024-07-01 13:53:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def recommend():
     recommended_emissions = {"Monday":[0, 0, 0], "Tuesday":[0, 0, 0], "Wednesday":[0, 0, 0], "Thursday":[0, 0, 0], "Friday":[0, 0, 0], 
                       "Saturday":[0, 0, 0], "Sunday":[0, 0, 0]}
 
-    print(np.isnan(selected_dishes["user_selected_dishes"].values)[0])
+    print(selected_dishes.iloc, selected_dishes.iloc[0])
     
     #loop over all the user-selected meals
     if not np.isnan(selected_dishes["user_selected_dishes"].values)[0]:
